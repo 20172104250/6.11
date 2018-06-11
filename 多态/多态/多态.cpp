0cebd18eddead0fc918b0e3dc99cd9cac6a6569b
20172104250 <2418665930@qq.com>
2018-06-11 16:25:00 +0800

    补充
    补全多态知识点的课堂练习

--- 多态/多态/多态.cpp
+++ 多态/多态/多态.cpp
@@ -2,10 +2,64 @@
 //
 
 #include "stdafx.h"
-
-
+#include "iostream"
+using namespace std;
+class CFeet
+{
+protected:
+	int feet;
+	int inches;
+public:
+	CFeet();
+	void setfeet(int f);
+	void setinches(int inc);
+	virtual void display();
+	int getfeet();
+	int getinches();
+};
+CFeet::CFeet()
+{
+	feet = 0;
+	inches = 0;
+}
+void CFeet::setfeet(int f)
+{
+	feet = f;
+}
+void CFeet::setinches(int inc)
+{
+	inches = inc % 12;
+	feet = feet + inc / 12;
+}
+void CFeet::display()
+{
+	cout << "»ùÀà" << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+class CMyfeet :public CFeet
+{
+public:
+	void display();
+};
+void CMyfeet::display()
+{
+	cout << "ÅÉÉúÀà" << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
+}
 int main()
 {
+	CFeet *p;
+	p = new CMyfeet;//CMyfeet Afeet p=&Afeet;
+	p->setfeet(3);
+	p->setinches(5);
+	p->display();
+	p->CFeet::display();
     return 0;
 }
 
